43e2d68ac74584a1672a67ce4449c33b0b11971b
Hugo Amnov <hugo.amiard@laposte.net>
2020-01-19 01:37:38 +0100

    Try bgfx example

--- docs/bgfx/example-01-cubesDebug.js
+++ docs/bgfx/example-01-cubesDebug.js
@@ -1233,11 +1233,11 @@ function updateGlobalBufferAndViews(buf) {
 }
 
 var STATIC_BASE = 1024,
-    STACK_BASE = 5917072,
+    STACK_BASE = 5910992,
     STACKTOP = STACK_BASE,
-    STACK_MAX = 674192,
-    DYNAMIC_BASE = 5917072,
-    DYNAMICTOP_PTR = 674032;
+    STACK_MAX = 668112,
+    DYNAMIC_BASE = 5910992,
+    DYNAMICTOP_PTR = 667952;
 
 assert(STACK_BASE % 16 === 0, 'stack must start aligned');
 assert(DYNAMIC_BASE % 16 === 0, 'heap must start aligned');
@@ -1747,7 +1747,7 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  550764: function() {debugger;}
+  544684: function() {debugger;}
 };
 
 // Avoid creating a new array
@@ -1779,7 +1779,7 @@ function _emscripten_asm_const_iii(code, sigPtr, argbuf) {
 
 
 
-// STATICTOP = STATIC_BASE + 673168;
+// STATICTOP = STATIC_BASE + 667088;
 /* global initializers */  __ATINIT__.push({ func: function() { ___wasm_call_ctors() } });
 
 
@@ -4419,7 +4419,7 @@ function _emscripten_asm_const_iii(code, sigPtr, argbuf) {
   function _emscripten_get_now() { abort() }
 
   function _emscripten_get_sbrk_ptr() {
-      return 674032;
+      return 667952;
     }
 
   
