9bd031b342035de5bc8c026ec84e433d4857b6b0
Hugo Amnov <hugo.amiard@laposte.net>
2020-02-01 21:26:41 +0100

    Try bgfx example

--- docs/bgfx/example-24-nbodyDebug.js
+++ docs/bgfx/example-24-nbodyDebug.js
@@ -195,7 +195,7 @@ Module['FS_createPath']('/shaders', 'spirv', true, true);
     }
   
    }
-   loadPackage({"files": [{"start": 0, "audio": 0, "end": 1985, "filename": "/shaders/spirv/vs_particle.bin"}, {"start": 1985, "audio": 0, "end": 3348, "filename": "/shaders/spirv/fs_particle.bin"}, {"start": 3348, "audio": 0, "end": 13909, "filename": "/shaders/spirv/cs_init_instances.bin"}, {"start": 13909, "audio": 0, "end": 17710, "filename": "/shaders/spirv/cs_update_instances.bin"}, {"start": 17710, "audio": 0, "end": 18936, "filename": "/shaders/spirv/cs_indirect.bin"}], "remote_package_size": 18936, "package_uuid": "c4b7c550-dfd1-4bd0-b5fd-156634a3d0b5"});
+   loadPackage({"files": [{"start": 0, "audio": 0, "end": 1985, "filename": "/shaders/spirv/vs_particle.bin"}, {"start": 1985, "audio": 0, "end": 3348, "filename": "/shaders/spirv/fs_particle.bin"}, {"start": 3348, "audio": 0, "end": 13909, "filename": "/shaders/spirv/cs_init_instances.bin"}, {"start": 13909, "audio": 0, "end": 17710, "filename": "/shaders/spirv/cs_update_instances.bin"}, {"start": 17710, "audio": 0, "end": 18936, "filename": "/shaders/spirv/cs_indirect.bin"}], "remote_package_size": 18936, "package_uuid": "3bcee238-0ece-46d2-a6fb-41d774934c85"});
   
   })();
   
@@ -6993,20 +6993,13 @@ function _emscripten_asm_const_iii(code, sigPtr, argbuf) {
   }
 
   function _wgpuCommandEncoderBeginComputePass(encoderId, descriptor) {
-      assert(descriptor);
-  
-      function makeComputePassDescriptor(descriptor) {
+      var desc;
+      if (descriptor) {
         assert(descriptor);assert(HEAP32[((descriptor)>>2)] === 0);
-        var desc = {
-          "label": undefined,
-        };
+        desc = {};
         var labelPtr = HEAP32[(((descriptor)+(4))>>2)];
         if (labelPtr) desc["label"] = UTF8ToString(labelPtr);
-        return desc;
       }
-  
-      var desc = makeComputePassDescriptor(descriptor);
-  
       var commandEncoder = WebGPU.mgrCommandEncoder.get(encoderId);
       return WebGPU.mgrComputePassEncoder.create(commandEncoder["beginComputePass"](desc));
     }
