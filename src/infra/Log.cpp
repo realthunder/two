7721a9894a566e6fa8af91697ed1fa732816be71
Hugo Amiard <hugo.amiard@laposte.net>
2020-05-03 20:28:46 +0200

    Fix Windows console build error

--- src/infra/Log.cpp
+++ src/infra/Log.cpp
@@ -15,6 +15,9 @@ module two.infra;
 
 #if defined _WIN32
 #include <windows.h>
+#ifndef ENABLE_VIRTUAL_TERMINAL_PROCESSING
+#define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
+#endif
 #endif
 
 namespace two
