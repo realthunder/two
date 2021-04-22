3a7e5c1e73663b4ce60d815f801a89d73cd1b8b7
Hugo Amiard <hugo.amiard@laposte.net>
2021-04-22 21:21:44 +0200

    Deactivate ReflectionAtlas

--- src/gfx-pbr/ReflectionAtlas.cpp
+++ src/gfx-pbr/ReflectionAtlas.cpp
@@ -21,13 +21,14 @@ namespace two
 		: m_size(size)
 		, m_subdiv(subdiv)
 	{
-#if 1 // @todo: crashes in D3D11
+		// TODO(hugoam) TextureSampler::ReflectionProbe collides with TextureSampler::Clusters
+#if 0 // @todo: crashes in D3D11
 		TextureFormat color_format = TextureFormat::RGBA16F;
 
 		if(!bgfx::isTextureValid(0, true, 1, bgfx::TextureFormat::Enum(color_format), TEXTURE_CLAMP))
 			color_format = TextureFormat::RGB10A2;
 
-		m_color = { uvec2(uint(size)), true, color_format, TEXTURE_CLAMP, true };
+		m_color = { uvec2(uint(size)), true, color_format, BGFX_TEXTURE_RT | TEXTURE_CLAMP, true };
 
 		for(int i = 0; i < 6; i++)
 		{
