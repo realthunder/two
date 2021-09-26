21c10a06c09bfe4b713ebb6354303e7242c738c2
Hugo Amiard <hugo.amiard@laposte.net>
2021-09-27 00:23:16 +0200

    Cleanup

--- src/type/Proto.h
+++ src/type/Proto.h
@@ -13,13 +13,13 @@
 
 namespace two
 {
-	/*	A Prototype represent a fixed layout of parts for an object, allowing for fast part query 
-			It is a broader definition of a type, different from the C++ class, it defines a finite object
-			with a finite set of capabilities, which is a sum of components : its parts
-
-			The parts are members of a prototype, whereas additionnal components are called plugs, 
-			and are not part of the object in itself : they can be here or not here, whereas the parts are always here
-	*/
+	// A Prototype represent a fixed layout of parts for an object, allowing for fast part query 
+	// It is a broader definition of a type, different from the C++ class, it defines a finite object
+	// with a finite set of capabilities, which is a sum of components : its parts
+	//
+	// The parts are members of a prototype, whereas additionnal components are called plugs, 
+	// and are not part of the object in itself : they can be here or not here, whereas the parts are always here
+	//
 
 	export_ class refl_ TWO_TYPE_EXPORT Prototype
 	{
